bf64f4071dc2795dbf0d9756e4a918a15687a50d
Arnaud Van Acker <arnaudvanacker@yahoo.fr>
2024-04-23 12:06:58 +0200

    switch sections

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,20 +1,31 @@
 //init
+const main=document.querySelector('main');
 const home=document.querySelector('#home');
 const menu=document.querySelector('#menu');
 const cart=document.querySelector('#cart');
 const about=document.querySelector('#about');
 
-
-
-//NAV
 const navArray=Array.from(document.querySelector('nav').children);
 const sectionArray=Array.from(document.querySelector('main').children);
+sectionArray.forEach(section => {
+    section.style.display='none';
+});
+home.style.display='block';
 
+//NAV
 for(let i=0; i<navArray.length; i++) {
     navArray[i].addEventListener('click', function() {
-        sectionArray.forEach(section => {
-            section.style.display='none';
-        });
-        sectionArray[i].style.display='block';
+        switchAnimation(sectionArray[i]);
     })
+}
+
+async function switchAnimation(section) {
+    main.classList.add('fadeOut');
+    await sleep(500);
+    sectionArray.forEach(section => {
+        section.style.display='none';
+    });
+    section.style.display='block';
+    main.classList.remove('fadeOut');
+    main.classList.add('fadeIn');
 }
