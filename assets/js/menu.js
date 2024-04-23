34901a0943bb82ebe8ea8ef532ca50324eea9bea
Arnaud Van Acker <arnaudvanacker@yahoo.fr>
2024-04-23 11:31:00 +0200

    menudata

---
+++ assets/js/menu.js
@@ -0,0 +1,19 @@
+const menuData=[
+    {
+        name:"Coco colo",
+        description:"Best cola of the world!",
+        picture:"",
+        price:"2",
+        category:"drink",
+        type:"soda"
+    },
+
+    {
+        name:"Jupilaes",
+        description:"Best beer of the world!",
+        picture:"",
+        price:"25",
+        category:"drink",
+        type:"alcool"
+    }
+]
